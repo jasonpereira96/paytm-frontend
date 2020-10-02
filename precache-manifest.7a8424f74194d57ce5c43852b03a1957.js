b80d3e44c8aba4b6471ef210b4e44a5b10cbc99c
jasonpereira96 <jasonpereira074@gmail.com>
2020-10-02 12:19:55 +0530

    Updates

---
+++ precache-manifest.7a8424f74194d57ce5c43852b03a1957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a41e0dc27861f1fd1640f48e0e3c59",
+    "url": "/paytm-frontend/index.html"
+  },
+  {
+    "revision": "b0fdba100efadecfa30f",
+    "url": "/paytm-frontend/static/css/main.d948dd78.chunk.css"
+  },
+  {
+    "revision": "78dbb27daa35e63b2ede",
+    "url": "/paytm-frontend/static/js/2.01be8ea7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/paytm-frontend/static/js/2.01be8ea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0fdba100efadecfa30f",
+    "url": "/paytm-frontend/static/js/main.423e0568.chunk.js"
+  },
+  {
+    "revision": "b6c006ae7f691b1cc9d9",
+    "url": "/paytm-frontend/static/js/runtime-main.868235de.js"
+  }
+]);
